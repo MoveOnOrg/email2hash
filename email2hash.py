7d0d5d6fac05b68e18d7af8c56fa1ae1600d2b83
Sukhbir Singh <sukhbir@riseup.net>
2018-06-30 11:00:38 -0400

    Randomly generate a secret for the user (fixes #2)
    If the user does not want to enter a secret of their own, we generate a
    random secret for them using a diceware passphrase. This is done using
    EFF's long wordlist (https://www.eff.org/dice).

--- email2hash.py
+++ email2hash.py
@@ -18,29 +18,43 @@ import os
 import sys
 import time
 import hmac
+import random
 import getpass
 import zipfile
 import hashlib
 import argparse
 
+DICEWARE = "wordlist.txt"
+
+
+def diceware_word():
+    with open(DICEWARE, "r") as f:
+        words = [line.split()[1] for line in f.readlines()]
+
+    return ' '.join(random.SystemRandom().choice(words) for _ in range(5))
+
 
 def get_secret():
+    random_secret = None
     while True:
-        secret = getpass.getpass("Enter the secret key: ")
+        secret = getpass.getpass("Enter the secret key "
+                                 "(or hit [ENTER] to generate a random key): ")
         if not secret:
-            print("! I need a secret to continue.")
-            continue
+            print("That's fine, I will generate a key for you and use that.")
+            secret = diceware_word()
+            random_secret = True
+            break
         # This is arbitrary; ideally the length should be at least 32 bytes
         # but we need to enforce a number.
         if len(secret) < 10:
-            print("! Please choose a secret longer than 10 characters.")
+            print("Please choose a secret longer than 10 characters.")
             continue
         confirm = getpass.getpass("Enter the same key again to confirm: ")
         if not secret == confirm:
-            print("! Your secret key did not match. Let's try again.")
+            print("Your secret key did not match. Let's try again.")
             continue
         break
-    return secret
+    return secret, random_secret
 
 
 def hash_email(args):
@@ -65,7 +79,7 @@ def hash_email(args):
                 sys.exit()
 
     # Input the secret key from the user.
-    secret = get_secret()
+    secret, random_secret = get_secret()
 
     if not args.silent:
         print("Please wait, hashing email addresses. This may take a while...")
@@ -118,6 +132,12 @@ def hash_email(args):
         print("Hashed {0} email addresses in {1:0.2f} seconds using {2} "
               "to {3}".format(index, end_time - start_time,
                               "HMAC (SHA3-256)", output_file))
+
+    if random_secret:
+        print("Your secret key (without quotes but spaces matter): "
+              "\"{0}\"".format(secret))
+        print("Please clear this screen or exit the terminal after you have "
+              "memorized the secret as it will not be displayed again.")
 
 
 def parse_args():
